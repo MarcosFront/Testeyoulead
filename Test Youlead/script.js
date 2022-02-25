58a16769a7961baca02383d01e6e358dabec2512
Marcos Vinicius Ribeiro Souza <73299025+MarcosFront@users.noreply.github.com>
2022-02-25 18:15:28 +0000

    Final Project

---
+++ Test Youlead/script.js
@@ -0,0 +1,42 @@
+const popup = document.querySelector('.pop-up');
+const overlay = document.getElementById('overlay')
+
+window.onload = function(){
+  setTimeout(function(){
+    popup.style.display = "none";
+    overlay.style.display = "none";
+  }, 4000)
+}
+
+function validateEmail(){
+    var email = document.querySelector('#email');
+    var error = document.querySelector('#error-email');
+    
+    if(!email.checkValidity()){
+      error.innerHTML = "Please Enter Valid Email";  
+    }     
+  }  
+  function resetMsg(){
+    var error = document.querySelector('#error-email');
+    if (error.innerHTML == "Please Enter Valid Email"){
+      error.innerHTML = "";
+    }
+  }
+
+const changeimg = document.querySelector('#sayhello')
+
+changeimg.addEventListener("mouseover", function(){
+    var img1 = document.getElementById('img-card1');
+    var img2 = document.getElementById('img-card2');
+    var img3 = document.getElementById('img-card3');
+    var imgh1 = document.getElementById('img-cardh-1');
+    var imgh2 = document.getElementById('img-cardh-2');
+    var imgh3 = document.getElementById('img-cardh-3');
+    imgh1.classList.remove("hide");
+    imgh2.classList.remove("hide");
+    imgh3.classList.remove("hide");
+    img1.classList.add("hide");
+    img2.classList.add("hide");
+    img3.classList.add("hide");
+    
+})
